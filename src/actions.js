2118ee2a17355a47c31f1afcf22a91f7beeca8b2
sbkn <sergeybakin87@googlemail.com>
2016-10-02 12:36:18 +0200

    replaced lodash.assign with es7 spread operator for objects, this works because of babel stage 2 preset

--- src/actions.js
+++ src/actions.js
@@ -3,7 +3,8 @@ import * as c from './constants';
 export function update(name, value) {
     return dispatch => dispatch({
         type: c.FORM_UPDATE_VALUE,
-        name, value
+        name,
+        value
     });
 }
 
--- src/store.js
+++ src/store.js
@@ -1,19 +1,24 @@
 import * as c from './constants';
-import assign from 'lodash.assign';
 
 const initialState = {
     values: {}
 };
 
+/*
+ * DO NOT EVER USE MUTATING METHODS SUCH AS arr.splice(),
+ * THAT IS WHY assign IS BEING USED HERE
+ */
 export default (state = initialState, action) => {
     switch (action.type) {
 
         case c.FORM_UPDATE_VALUE:
-            return assign({}, state, {
-                values: assign({}, state.values, {
+            return {
+                ...state,
+                values: {
+                    ...state.values,
                     [action.name]: action.value
-                })
-            });
+                }
+            };
 
         case c.FORM_RESET:
             return initialState;
